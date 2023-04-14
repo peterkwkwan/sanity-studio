25e702c0f25ff38ddceb121c4323b4424856d7f7
peterkwkwan <peterkwkwan@gmail.com>
2023-04-15 00:51:13 +0800

    feat: charCount to render out maxCharCount warning

--- components/characterCount.tsx
+++ components/characterCount.tsx
@@ -1,16 +1,27 @@
 import {Stack, Text} from '@sanity/ui'
-import {InputProps} from 'sanity'
+import {InputProps, RuleSpec} from 'sanity'
 
 export default function CharacterCount(props: InputProps) {
   if (props.schemaType?.options?.showCount) {
-    return (
-      <Stack space={2}>
-        {props.renderDefault(props)}
-        <Text size={1} align="left">
-          Length: {(props?.value as string)?.length}
-        </Text>
-      </Stack>
-    )
+    // validation -> _rules - contains all validation rules
+
+    const validation: unknown[] = props.schemaType?.validation as any
+
+    if (validation && validation[0]) {
+      const rules = (validation[0] as any)._rules
+
+      const maxCharCount = rules.find((rule: RuleSpec) => rule.flag === 'max').constraint
+
+      return (
+        <Stack space={2}>
+          {props.renderDefault(props)}
+          <Text size={1} align="left" style={{color: 'red'}}>
+            {(props?.value as string)?.length > maxCharCount &&
+              `${props.schemaType.title} cannot be greater than ${maxCharCount} characters`}
+          </Text>
+        </Stack>
+      )
+    }
   }
 
   return props.renderDefault(props)
