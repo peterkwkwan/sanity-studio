3632c762d8b0e25f20ed3e1092df9958388c2a23
peterkwkwan <peterkwkwan@gmail.com>
2023-04-16 15:21:40 +0800

    feat: remove documentPane for competencies

--- desk/defaultDocumentNode.ts
+++ desk/defaultDocumentNode.ts
@@ -3,20 +3,20 @@ import DocumentsPane from 'sanity-plugin-documents-pane'
 
 export const defaultDocumentNode: DefaultDocumentNodeResolver = (S, {schemaType}) => {
   switch (schemaType) {
-    case `competencies`:
-      return S.document().views([
-        S.view.form(),
-        S.view
-          .component(DocumentsPane)
-          .options({
-            query: `*[
-              _type == "practiceArea" &&
-              references($id)
-            ]`,
-            params: {id: `_id`},
-          })
-          .title('Practice Area(s)'),
-      ])
+    // case `competencies`:
+    //   return S.document().views([
+    //     S.view.form(),
+    //     S.view
+    //       .component(DocumentsPane)
+    //       .options({
+    //         query: `*[
+    //           _type == "competenciesPage" &&
+    //           references($id)
+    //         ]`,
+    //         params: {id: `_id`},
+    //       })
+    //       .title('Practice Area(s)'),
+    //   ])
     default:
       return S.document().views([S.view.form()])
   }
