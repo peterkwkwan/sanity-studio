7f577c55f447bd8a38a1969033622dcdbe4fafc1
peterkwkwan <peterkwkwan@gmail.com>
2023-04-16 15:22:41 +0800

    feat: comment for todo on document pane

--- desk/defaultDocumentNode.ts
+++ desk/defaultDocumentNode.ts
@@ -3,6 +3,9 @@ import DocumentsPane from 'sanity-plugin-documents-pane'
 
 export const defaultDocumentNode: DefaultDocumentNodeResolver = (S, {schemaType}) => {
   switch (schemaType) {
+    // TO-DO: should render out all practice areas that are referenced by this competency
+    // groq query needs to be tweaked? Not sure if possible
+
     // case `competencies`:
     //   return S.document().views([
     //     S.view.form(),
