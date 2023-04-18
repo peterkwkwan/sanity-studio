3b5186ecfd930cf6e3467fb81c87aa259e7f87a1
peterkwkwan <peterkwkwan@gmail.com>
2023-04-18 22:29:56 +0800

    feat: add iframe for live previews

--- desk/defaultDocumentNode.ts
+++ desk/defaultDocumentNode.ts
@@ -1,8 +1,25 @@
 import type {DefaultDocumentNodeResolver} from 'sanity/desk'
+import Iframe from 'sanity-plugin-iframe-pane'
 import DocumentsPane from 'sanity-plugin-documents-pane'
+import {SanityDocument, Slug} from 'sanity'
 
 export const defaultDocumentNode: DefaultDocumentNodeResolver = (S, {schemaType}) => {
   switch (schemaType) {
+    case `homePage`:
+      return S.document().views([
+        S.view.form(),
+        S.view
+          // enable live previews
+          // READ: https://www.sanity.io/guides/nextjs-live-preview#0444f4dd6f03
+          .component(Iframe)
+          .options({
+            url: (doc: SanityDocument) =>
+              (doc?.slug as Slug)?.current
+                ? `http://localhost:3000/api/preview?slug=${doc.slug.current}`
+                : `http://localhost:3000/api/preview`,
+          })
+          .title('Live Preview'),
+      ])
     // TO-DO: should render out all practice areas that are referenced by this competency
     // groq query needs to be tweaked? Not sure if possible
 
