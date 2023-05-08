693b5376e1b49a17d5d8d7d7f92a02c06b4f0ee9
peterkwkwan <peterkwkwan@gmail.com>
2023-05-08 22:34:36 +0800

    feat: isUniqueString

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,30 +1,29 @@
 import {CurrentUser, ValidationContext} from 'sanity'
-import {useClient} from 'sanity'
 
 export const isAdministrator = (currentUser: Omit<CurrentUser, 'role'> | null) => {
   return !!currentUser?.roles.some((role) => role.name === 'administrator')
 }
 
-// type IsUniqueString = (value: string, context: ValidationContext) => Promise<any>
+type IsUniqueString = (value: string, context: ValidationContext) => Promise<any>
 
-// export const useIsUniqueString: IsUniqueString = (value, context) => {
-//   const client = useClient()
+export const isUniqueString: IsUniqueString = (value, context) => {
+  const {getClient, document, path = []} = context
 
-//   const {document} = context
+  const client = getClient({apiVersion: '2023-05-01'})
 
-//   const id = document?._id.replace(/^drafts\./, '')
+  const id = document?._id.replace(/^drafts\./, '')
 
-//   const params = {
-//     draft: `drafts.${id}`,
-//     published: id,
-//     value,
-//   }
+  const params = {
+    draft: `drafts.${id}`,
+    published: id,
+    value,
+  }
 
-//   const query = `!defined(*[
-//       _type == 'competencies' &&
-//       !(_id in [$draft, $published]) &&
-//       title == $value
-//     ][0]._id)`
+  const query = `!defined(*[
+      _type == '${document?._type}' &&
+      !(_id in [$draft, $published]) &&
+      lower(${path[0]}) == lower($value)
+    ][0]._id)`
 
-//   return client.fetch(query, params)
-// }
+  return client.fetch(query, params)
+}
